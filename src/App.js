7b0c222f0b74c2255a4d2a61cc08086a3d35e464
D Roemer <dominicroemer@gmail.com>
2023-02-22 15:56:35 -0800

    Add and style footer attribution correctly to adjust based on viewport size and content - finally figured out the height and min-height properties

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      <div id='page-container'>
+        <div id='content-wrap'>
+          <h1 className="sr-only">Card Details Form Interactive</h1>
+
+          <div className='component-container'>
+            {/* <NewsHomepage/> */}
+    
+            {/* And the content goes here. */}
+
+            I am the page's content
+
+          </div>
+
+          <footer className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io/profile/dominicroemer" target="_blank" rel='noreferrer'>Frontend Mentor</a>. 
+          Coded by <a href="https://github.com/dianaroemer">Diana Roemer</a>.
+          </footer>
+
+
+        </div>
+      </div>
+    </main>
   );
 }
 
